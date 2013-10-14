16e8d90231177e3aaf84945e2059b4266bada60d
thetekst <thetekst@gmail.com>
2013-10-14 23:50:13 +0400

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$langPrefix = 'ru';
+//$langPrefix = 'en';
+?>
+
+<!doctype html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">
+
+		<title>HTML5</title>
+
+		<script src="js/text_<?=$langPrefix;?>.js"></script>
+	</head>
+	<body>
+		<p id="mes1"></p>
+		<p id="mes2"></p>
+		<p id="mes3"></p>
+		<script>
+			document.getElementById('mes1').innerHTML = window.tr.mes1;
+			document.getElementById('mes2').innerHTML = window.tr.mes2;
+			document.getElementById('mes3').innerHTML = window.tr.mes3;
+		</script>
+	</body>
+</html>
---
+++ js/text_en.js
@@ -0,0 +1,5 @@
+window.tr = {
+	mes1:"Message 1",
+	mes2:"Message 2",
+	mes3:"Message 3"
+};
