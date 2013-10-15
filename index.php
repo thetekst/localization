6203b292cfa5568414d5b5f14aac4f71d52607ce
Dima work <thetekst@gmail.com>
2013-10-15 09:55:44 +0400

    способ 2

--- index.php
+++ index.php
@@ -16,10 +16,19 @@ $langPrefix = 'ru';
 		<p id="mes1"></p>
 		<p id="mes2"></p>
 		<p id="mes3"></p>
-		<script>
+		
+		<!--One-->
+		<!--script>
 			document.getElementById('mes1').innerHTML = window.tr.mes1;
 			document.getElementById('mes2').innerHTML = window.tr.mes2;
 			document.getElementById('mes3').innerHTML = window.tr.mes3;
+		</script-->
+		
+		<!--Two-->
+		<script>
+			for (var key in window.tr) {
+				document.getElementsByTagName('p')[key].innerHTML = window.tr[key];
+			}
 		</script>
 	</body>
 </html>
